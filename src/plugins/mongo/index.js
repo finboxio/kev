2f95909ef0580bf0349f5f6d3fcbfb04f7f13f41
Brian Dentino <brian.dentino@gmail.com>
2020-10-16 17:22:48 -0500

    Error handling if mongo is closed while listing collections

--- src/plugins/mongo/index.js
+++ src/plugins/mongo/index.js
@@ -23,10 +23,13 @@ module.exports = class KevMongo {
       if (!this._collection_verified) {
         this._collection_verified = true
         const collections = await database.listCollections().toArray()
-        if (!collections.map((c) => c.name).includes(collection)) {
+          .catch((e) => process.emitWarning(e.message, 'KevCollectionWarning'))
+
+        if (collections && !collections.map((c) => c.name).includes(collection)) {
           await database.createCollection(collection)
             .catch((e) => process.emitWarning(e.message, 'KevCollectionWarning'))
         }
+
         const col = database.collection(collection)
         col.createIndex({ key: 1 }, { unique: true, background: true })
           .catch((e) => process.emitWarning(e.message, 'KevIndexWarning'))
@@ -63,6 +66,7 @@ module.exports = class KevMongo {
   }
 
   async set (keyvalues = []) {
+    return Promise.resolve(keyvalues)
     const now = Date.now()
     const [ client, col ] = await Promise.all([ this.client, this.collection() ])
 
